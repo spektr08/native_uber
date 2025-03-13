1e127dabf6d19f8e7a913ee841f31e4711114f05
dima <dmprog08@gmail.com>
2025-03-13 15:31:33 +0200

    uver

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Page = () => {
+  <Text>auth</Text>;
+};
+
+export default Page;
---
+++ lib/map.ts
@@ -0,0 +1,193 @@
+import { Driver, MarkerData } from "@/types/type";
+
+const directionsAPI = process.env.EXPO_PUBLIC_DIRECTIONS_API_KEY;
+
+export const generateMarkersFromData = ({
+  data,
+  userLatitude,
+  userLongitude,
+}: {
+  data: Driver[];
+  userLatitude: number;
+  userLongitude: number;
+}): MarkerData[] => {
+  return data.map((driver) => {
+    const latOffset = (Math.random() - 0.5) * 0.01; // Random offset between -0.005 and 0.005
+    const lngOffset = (Math.random() - 0.5) * 0.01; // Random offset between -0.005 and 0.005
+
+    return {
+      latitude: userLatitude + latOffset,
+      longitude: userLongitude + lngOffset,
+      title: `${driver.first_name} ${driver.last_name}`,
+      ...driver,
+    };
+  });
+};
+
+export const calculateRegion = ({
+  userLatitude,
+  userLongitude,
+  destinationLatitude,
+  destinationLongitude,
+}: {
+  userLatitude: number | null;
+  userLongitude: number | null;
+  destinationLatitude?: number | null;
+  destinationLongitude?: number | null;
+}) => {
+  if (!userLatitude || !userLongitude) {
+    return {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    };
+  }
+
+  if (!destinationLatitude || !destinationLongitude) {
+    return {
+      latitude: userLatitude,
+      longitude: userLongitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    };
+  }
+
+  const minLat = Math.min(userLatitude, destinationLatitude);
+  const maxLat = Math.max(userLatitude, destinationLatitude);
+  const minLng = Math.min(userLongitude, destinationLongitude);
+  const maxLng = Math.max(userLongitude, destinationLongitude);
+
+  const latitudeDelta = (maxLat - minLat) * 1.3; // Adding some padding
+  const longitudeDelta = (maxLng - minLng) * 1.3; // Adding some padding
+
+  const latitude = (userLatitude + destinationLatitude) / 2;
+  const longitude = (userLongitude + destinationLongitude) / 2;
+
+  return {
+    latitude,
+    longitude,
+    latitudeDelta,
+    longitudeDelta,
+  };
+};
+
+export const calculateDriverTimes = async ({
+  markers,
+  userLatitude,
+  userLongitude,
+  destinationLatitude,
+  destinationLongitude,
+}: {
+  markers: MarkerData[];
+  userLatitude: number | null;
+  userLongitude: number | null;
+  destinationLatitude: number | null;
+  destinationLongitude: number | null;
+}) => {
+  if (
+    !userLatitude ||
+    !userLongitude ||
+    !destinationLatitude ||
+    !destinationLongitude
+  )
+    return;
+
+  try {
+    const timesPromises = markers.map(async (marker) => {
+      const responseToUser = await fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${marker.latitude},${marker.longitude}&destination=${userLatitude},${userLongitude}&key=${directionsAPI}`,
+      );
+      const dataToUser = await responseToUser.json();
+      const timeToUser = dataToUser.routes[0].legs[0].duration.value; // Time in seconds
+
+      const responseToDestination = await fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${userLatitude},${userLongitude}&destination=${destinationLatitude},${destinationLongitude}&key=${directionsAPI}`,
+      );
+      const dataToDestination = await responseToDestination.json();
+      const timeToDestination =
+        dataToDestination.routes[0].legs[0].duration.value; // Time in seconds
+
+      const totalTime = (timeToUser + timeToDestination) / 60; // Total time in minutes
+      const price = (totalTime * 0.5).toFixed(2); // Calculate price based on time
+
+      return { ...marker, time: totalTime, price };
+    });
+
+    return await Promise.all(timesPromises);
+  } catch (error) {
+    console.error("Error calculating driver times:", error);
+  }
+};
+
+const decodePolyline = (encoded: string) => {
+  let points = [];
+  let index = 0,
+    len = encoded.length;
+  let lat = 0,
+    lng = 0;
+
+  while (index < len) {
+    let b,
+      shift = 0,
+      result = 0;
+    do {
+      b = encoded.charCodeAt(index++) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+    const dlat = result & 1 ? ~(result >> 1) : result >> 1;
+    lat += dlat;
+
+    shift = 0;
+    result = 0;
+    do {
+      b = encoded.charCodeAt(index++) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+    const dlng = result & 1 ? ~(result >> 1) : result >> 1;
+    lng += dlng;
+
+    points.push({ latitude: lat / 1e5, longitude: lng / 1e5 });
+  }
+  return points;
+};
+
+export async function calculateDirections(userLatitude:number | null, userLongitude:number | null, destinationLatitude:number | null, destinationLongitude:number | null){
+  const directionsAPI = process.env.EXPO_PUBLIC_DIRECTIONS_API_KEY;
+  const url = `https://routes.googleapis.com/directions/v2:computeRoutes`;
+  
+  const body = {
+    origin: {
+      location: { latLng: { latitude: userLatitude, longitude: userLongitude } }
+    },
+    destination: {
+      location: { latLng: { latitude: destinationLatitude, longitude: destinationLongitude } }
+    },
+    travelMode: "DRIVE",
+    routingPreference: "TRAFFIC_AWARE",
+    computeAlternativeRoutes: false
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": directionsAPI!,
+        "X-Goog-FieldMask": "routes.duration,routes.distanceMeters,routes.polyline"
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+    if (data.routes && data.routes.length > 0) {
+      const polylinePoints = decodePolyline(data.routes[0].polyline.encodedPolyline);
+      return polylinePoints;
+    }
+  } catch (error) {
+    console.error("Error fetching route:", error);
+  }
+
+}
